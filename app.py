26c43e7196bd95781a1a8e5e3e739a4860666360
Mohamed Hussein <150954700+MohamedHussein811@users.noreply.github.com>
2024-09-27 00:52:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def predict(model_key):
         'prediction_percentages': prediction_percentages
     })
 if __name__ == '__main__':
-    #load_keras_models() 
+    load_keras_models() 
     app.run()
